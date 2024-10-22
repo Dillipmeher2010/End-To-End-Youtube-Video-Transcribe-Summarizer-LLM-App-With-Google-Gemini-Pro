737ce0ff4917e579c3ec09c3c60ff3a907f09b5e
DILLIP MEHER2010 <dillipmeher2010@gmail.com>
2024-10-22 06:21:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ load_dotenv() ##load all the nevironment variables
 import os
 import google.generativeai as genai
 
-from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
+from youtube_transcript_api import YouTubeTranscriptApi
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
